7ac7af29048b2377cea4d42c51652813d4329603
BradockBr <robtognetti@gmail.com>
2023-05-26 16:47:29 -0300

    req 1, 2 , 3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+
+        return None
+
+    horas = 0
+
+    for start, finish in permanence_period:
+        if type(start) is not int or type(finish) is not int:
+            return None
+
+        if start <= target_time <= finish:
+            horas += 1
+
+    return horas
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "jubileu"
+    assert encrypt_message(message, 2) == ("uelib_uj")
+    assert encrypt_message(message, 7) == ("uelibuj")
+
+    with pytest.raises(TypeError):
+        encrypt_message(message, "")
